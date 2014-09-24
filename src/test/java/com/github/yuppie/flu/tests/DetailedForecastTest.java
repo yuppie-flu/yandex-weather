84e056df689d76dbd83d7ddd2fb83fced99b5d8b
Kirill Kozlov <kozlov.k.e@gmail.com>
2014-09-24 23:41:48 +0400

    [*] Check short weekday names in detailed forecast

--- src/test/java/com/github/yuppie/flu/tests/DetailedForecastTest.java
+++ src/test/java/com/github/yuppie/flu/tests/DetailedForecastTest.java
@@ -10,14 +10,17 @@ import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.text.DateFormatSymbols;
 import java.util.List;
+import java.util.Locale;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
 
 /**
- * TODO: add description
+ * Checks detailed forecast report.
  *
  * @author Kirill Kozlov
  * @since 24.09.2014
@@ -26,8 +29,12 @@ public class DetailedForecastTest extends YandexWeatherBaseTest {
     /*===========================================[ STATIC VARIABLES ]=============*/
     private static final Logger LOGGER = LoggerFactory.getLogger(DetailedForecastTest.class);
 
-    public static final int TODAY_SCROLL_SIZE = 10;
-    public static final int TOMORROW_SCROLL_SIZE = 9;
+    private static final int TODAY_SCROLL_SIZE = 10;
+    private static final int TOMORROW_SCROLL_SIZE = 9;
+
+    private static final String[] RUSSIAN_SHORT_WEEKDAYS = {
+            "", "пн", "вт", "ср", "чт", "пт", "сб", "вс"
+    };
     /*===========================================[ INSTANCE VARIABLES ]===========*/
     private DetailedForecastPage detailedForecastPage;
     /*===========================================[ CONSTRUCTORS ]=================*/
@@ -59,6 +66,8 @@ public class DetailedForecastTest extends YandexWeatherBaseTest {
             LocalDate expectedDate = startDate.plusDays(i);
 
             assertThat("Incorrect date", actualDate, equalTo(expectedDate));
+            String expectedDayName = RUSSIAN_SHORT_WEEKDAYS[expectedDate.getDayOfWeek()];
+            assertThat("Incorrect short name of weekday", df.getDayName(), equalTo(expectedDayName));
         }
     }
 }
